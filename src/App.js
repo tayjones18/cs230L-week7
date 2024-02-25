2fc01cd5495a61817691b74cd1c9351297262a6f
tayjones18 <tej00006@mix.wvu.edu>
2024-02-25 16:31:53 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 001</h2>
+      <p>WVU ID: 800392027</p>
+      <p>Hi I am Taylor</p>
+    </div>
+  );
+}
+
+export default App;
