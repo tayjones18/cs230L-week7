fe46e8cd0511aa59e36123fbf5bcaa80f8a5e91e
tayjones18 <tej00006@mix.wvu.edu>
2024-03-01 15:50:37 -0500

    week 8 attempt 8

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,25 @@
         
 const Card = () => {  
     return (
-        <div className = "card">
-            <h2>Card 01</h2>
-            <p>This is some card content</p>
+        <div class="card-deck">
+        <div class="card">
+            <div class="card-body">
+            <h5 class="card-title">Card 01</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+            <h5 class="card-title">Card 02</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+            <h5 class="card-title">Card 03</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
         </div>
     );
 };
