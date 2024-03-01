2920881b7efb2674473f1b2334d186a02c796d4a
tayjones18 <tej00006@mix.wvu.edu>
2024-03-01 16:29:04 -0500

    week 8 attempt 11

--- src/Card.js
+++ src/Card.js
@@ -1,37 +1,49 @@
         
 const Card = () => {  
     return (
-        <div class="card-deck">
-        <div class="card">
-        <img class="card-img-top" src="..." alt="Card image cap"/>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-      </div>
-      <div class="card">
-        <img class="card-img-top" src="..." alt="Card image cap"/>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-        </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-      </div>
-      <div class="card">
-        <img class="card-img-top" src="..." alt="Card image cap"/>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-        </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-      </div>
+        <div class="container"> 
+        <div class="row"> 
+            <div class="col-lg-6 mb-4"> 
+                <div class="card"> 
+                    <img class="card-img-top" src="" alt=""/> 
+  
+                    <div class="card-body"> 
+                        <h5 class="card-title">Card title</h5> 
+                        <p class="card-text"> 
+                            Some quick example text to build on  
+                            the card title and make up the bulk  
+                            of the card's content. 
+                        </p> 
+  
+                        <a href="#" class="btn btn-outline-primary btn-sm"> 
+                            Card link 
+                        </a> 
+                        <a href="#" class="btn btn-outline-secondary btn-sm"> 
+                            <i class="far fa-heart"></i></a> 
+                    </div> 
+                </div> 
+            </div> 
+            <div class="col-lg-6 mb-4"> 
+                <div class="card"> 
+                    <img class="card-img-top" src="" alt=""/> 
+  
+                    <div class="card-body"> 
+                        <h5 class="card-title">Card title</h5> 
+                        <p class="card-text"> 
+                            Some quick example text to build on the  
+                            card title and make up the bulk of the  
+                            card's content. 
+                        </p> 
+                          
+                        <a href="#" class="btn btn-outline-primary btn-sm"> 
+                            Card link 
+                        </a> 
+                        <a href="#" class="btn btn-outline-secondary btn-sm"> 
+                            <i class="far fa-heart"></i></a> 
+                    </div> 
+                </div> 
+            </div> 
+        </div> 
     </div>
     );
 };
