a2961710dc4a09e6311b0c02ede7460e7760e422
tayjones18 <tej00006@mix.wvu.edu>
2024-03-01 16:37:15 -0500

    week 8 attempt 12

--- src/Card.js
+++ src/Card.js
@@ -4,42 +4,44 @@ const Card = () => {
         <div class="container"> 
         <div class="row"> 
             <div class="col-lg-6 mb-4"> 
-                <div class="card"> 
+                <div class="card text-white bg-primary mb-3" style="max-width: 18rem;"> 
                     <img class="card-img-top" src="" alt=""/> 
   
                     <div class="card-body"> 
-                        <h5 class="card-title">Card title</h5> 
+                        <h5 class="card-title">Card 01</h5> 
                         <p class="card-text"> 
                             Some quick example text to build on  
                             the card title and make up the bulk  
                             of the card's content. 
                         </p> 
-  
-                        <a href="#" class="btn btn-outline-primary btn-sm"> 
-                            Card link 
-                        </a> 
-                        <a href="#" class="btn btn-outline-secondary btn-sm"> 
-                            <i class="far fa-heart"></i></a> 
                     </div> 
                 </div> 
             </div> 
             <div class="col-lg-6 mb-4"> 
-                <div class="card"> 
+                <div class="card text-white bg-danger mb-3" style="max-width: 18rem;"> 
                     <img class="card-img-top" src="" alt=""/> 
   
                     <div class="card-body"> 
-                        <h5 class="card-title">Card title</h5> 
+                        <h5 class="card-title">Card 02</h5> 
                         <p class="card-text"> 
                             Some quick example text to build on the  
                             card title and make up the bulk of the  
                             card's content. 
                         </p> 
-                          
-                        <a href="#" class="btn btn-outline-primary btn-sm"> 
-                            Card link 
-                        </a> 
-                        <a href="#" class="btn btn-outline-secondary btn-sm"> 
-                            <i class="far fa-heart"></i></a> 
+                    </div> 
+                </div> 
+            </div> 
+            <div class="col-lg-6 mb-4"> 
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;"> 
+                    <img class="card-img-top" src="" alt=""/> 
+  
+                    <div class="card-body"> 
+                        <h5 class="card-title">Card 03</h5> 
+                        <p class="card-text"> 
+                            Some quick example text to build on the  
+                            card title and make up the bulk of the  
+                            card's content. 
+                        </p> 
                     </div> 
                 </div> 
             </div> 
