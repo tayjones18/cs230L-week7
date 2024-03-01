e7d9d5c71a09a62ec0d3cc64f1f8db151baab4dc
tayjones18 <tej00006@mix.wvu.edu>
2024-03-01 18:44:21 -0500

    week 8 attempt 15

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ const Card = () => {
         <div class="container"> 
         <div class="row"> 
             <div class="col-lg-6 mb-4"> 
-                <div class="card text-white bg-primary mb-3 w-40">   
+                <div class="card text-white bg-primary mb-3" style = "width: 18rem">   
                     <div class="card-body"> 
                         <h5 class="card-title">Card 01</h5> 
                         <p class="card-text"> 
@@ -16,7 +16,7 @@ const Card = () => {
                 </div> 
             </div> 
             <div class="col-lg-6 mb-4"> 
-                <div class="card text-white bg-danger mb-3 w-40"> 
+                <div class="card text-white bg-danger mb-3" style = "width: 18rem"> 
                     <div class="card-body"> 
                         <h5 class="card-title">Card 02</h5> 
                         <p class="card-text"> 
@@ -28,7 +28,7 @@ const Card = () => {
                 </div> 
             </div> 
             <div class="col-lg-6 mb-4"> 
-                <div class="card text-white bg-success mb-3 w-40"> 
+                <div class="card text-white bg-success mb-3" style = "width: 18rem"> 
                     <div class="card-body"> 
                         <h5 class="card-title">Card 03</h5> 
                         <p class="card-text"> 
