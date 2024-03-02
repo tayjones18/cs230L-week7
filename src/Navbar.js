71f774b556b5cc67891e767bbd9880d4246a1124
tayjones18 <tej00006@mix.wvu.edu>
2024-03-01 19:28:30 -0500

    week 8 attempt 24

--- src/Navbar.js
+++ src/Navbar.js
@@ -26,13 +26,11 @@ const Navbar = () => {
                 Contact
               </a>
             </li>
-            <li>
+            </ul>
                 <form class="form-inline m-2 my-lg-0">
                     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
-            </li>
-          </ul>
         </div>
         </nav>
     );
