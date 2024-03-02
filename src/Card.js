110c581fb8db6e546da92e978ee9c6a2308a7108
tayjones18 <tej00006@mix.wvu.edu>
2024-03-01 19:08:22 -0500

    week 8 attempt 20

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,8 @@
         
 const Card = () => {  
     return (
-        <div class="card-deck"> 
+        <div class = "container">
+        <div class="card-group"> 
             <div class="col-lg-6 mb-4"> 
                 <div class="card text-white bg-primary mb-3 w-50">   
                     <div class="card-body"> 
@@ -39,6 +40,7 @@ const Card = () => {
                 </div> 
             </div> 
         </div> 
+    </div>
     );
 };
 
